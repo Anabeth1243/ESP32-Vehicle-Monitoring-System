d1b51567938f1df305cfee709e502e914b853f2a
Anabeth <18124590403@163.com>
2026-09-04 01:19:22 +0800

    Update read_flash.ino

--- Firmware/read_flash/read_flash.ino
+++ Firmware/read_flash/read_flash.ino
@@ -1,1 +1,35 @@
+#include <LittleFS.h>
 
+void setup() {
+
+  Serial.begin(115200);
+
+  // Initialize LittleFS
+  if (!LittleFS.begin(true)) {
+    Serial.println("LittleFS failed");
+    while (1);
+  }
+
+  // Open the recorded CSV file
+  File file = LittleFS.open("/temperature.csv", "r");
+
+  if (!file) {
+    Serial.println("File not found");
+    return;
+  }
+
+  Serial.println("----- DATA START -----");
+
+  // Read and print the CSV file
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+
+  file.close();
+
+  Serial.println();
+  Serial.println("----- DATA END -----");
+}
+
+void loop() {
+}
